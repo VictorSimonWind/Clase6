dd9bd86f7a78218572447a26cf6d58d408eb95ea
Victor Cequea Mijares <victor.cequeamijares.ext@bayer.com>
2021-07-08 17:31:28 -0300

    Promises

--- src/components/Promises.js
+++ src/components/Promises.js
@@ -1,54 +1,97 @@
+/**JAVASCRIPT ES SINCRONICO Y ES Y SE MANEJA DE UN HILO A LA VEZ*/
+
+/**muchas veses vamos a sentir que muchas cosas ocurren a la vez
+ * debido a la velocidad a la que los eventos se disparan, pero
+ * los eventos, pero el codigo siempre se lee de arriba hacia abajo,
+ * y cuando un bloque de codigo se ejecuta, no hay otro que se ejecute a la vez.
+ * 
+ * para indagar mas en el tema, puedes leer el articulo debajo
+ * 
+ * https://stackoverflow.com/questions/2035645/when-is-javascript-synchronous
+ * 
+ * a todo esto, JS tiene Excepciones a este comportamiento, 
+ * que le permiten comportarse de forma Syncronica estos son CallBacks
+ * Ejemplos
+ * setTimeout();
+ * fetch();
+ * 
+ */
 import React from 'react'
 import {useState, useEffect} from 'react'
 
 export default function Promises() {
 
 const [products, setProducts] = useState([]);
+
 useEffect(()=>{
-    getProducts()
+   
+    getProducts();
+   
 })
 
-const getProducts = () =>{
+const getProducts = async () =>{
 
-        let resolve;
-        //emulamos llamada al servidor
-        setTimeout(()=>{
-            /** el retorno de una llamada suele durar 
-             * varios segundos, este caso lo emulamos 
-             * con un Timeout */
-            let resolve = "[{\"id\":1,\"image\":\"https://images-na.ssl-images-amazon.com/images/I/81VBPk5pnLL._AC_SL1500_.jpg\"},{\"id\":1,\"image\":\"https://images-na.ssl-images-amazon.com/images/I/81VBPk5pnLL._AC_SL1500_.jpg\"},{\"id\":1,\"image\":\"https://images-na.ssl-images-amazon.com/images/I/81VBPk5pnLL._AC_SL1500_.jpg\"},{\"id\":1,\"image\":\"https://images-na.ssl-images-amazon.com/images/I/81VBPk5pnLL._AC_SL1500_.jpg\"},{\"id\":1,\"image\":\"https://images-na.ssl-images-amazon.com/images/I/81VBPk5pnLL._AC_SL1500_.jpg\"},{\"id\":1,\"image\":\"https://images-na.ssl-images-amazon.com/images/I/81VBPk5pnLL._AC_SL1500_.jpg\"}]"
-            console.log(resolve);
-        },3000);
-            /**al pedirle que convierta la respuesta de
-             * nuestro servidor en un objecto JSON para
-             * empezar a trabajar con, la aplicacion se 
-             * rompe, por que si bien JavaScript inicio
-             * la llamada al servidor, no espero a tener 
-             * un resultado para seguir con el resto de la
-             * aplicacion.
-             * ESTO UN COMPORTAMIENTO ASYNCRONO
-            */
-         resolve = JSON.parse(resolve);
-         setProducts(resolve);
-
+//        let resolve;
+//        //emulamos llamada al servidor
+//        setTimeout(()=>{
+//            /** el retorno de una llamada suele durar 
+//             * varios segundos, este caso lo emulamos 
+//             * con un Timeout */
+//            let resolve = "[{\"id\":1,\"image\":\"https://images-na.ssl-images-amazon.com/images/I/81VBPk5pnLL._AC_SL1500_.jpg\"},{\"id\":1,\"image\":\"https://images-na.ssl-images-amazon.com/images/I/81VBPk5pnLL._AC_SL1500_.jpg\"},{\"id\":1,\"image\":\"https://images-na.ssl-images-amazon.com/images/I/81VBPk5pnLL._AC_SL1500_.jpg\"},{\"id\":1,\"image\":\"https://images-na.ssl-images-amazon.com/images/I/81VBPk5pnLL._AC_SL1500_.jpg\"},{\"id\":1,\"image\":\"https://images-na.ssl-images-amazon.com/images/I/81VBPk5pnLL._AC_SL1500_.jpg\"},{\"id\":1,\"image\":\"https://images-na.ssl-images-amazon.com/images/I/81VBPk5pnLL._AC_SL1500_.jpg\"}]"
+//            console.log(resolve);
+//            },3000);
+//            /**al pedirle que convierta la respuesta de
+//             * nuestro servidor en un objecto JSON para
+//             * empezar a trabajar, la aplicacion se 
+//             * rompe, por que si bien JavaScript inicio
+//             * la llamada al servidor, no espero a tener 
+//             * un resultado para seguir con el resto de la
+//             * aplicacion.
+//             * ESTO UN COMPORTAMIENTO ASYNCRONO
+//            */
+//         resolve = JSON.parse(resolve);
+//         setProducts(resolve);
+//
          
-    
-/** 
-        new Promise((resolve,reject)=>{
-                //emulamos llamada al servidor
+//        let ServerOnline = true;
+//      new Promise((resolve,reject)=>{
+//              //emulamos llamada al servidor
+//          setTimeout(()=>{
+//              if(ServerOnline){
+//                  resolve("[{\"id\":1,\"image\":\"https://images-na.ssl-images-amazon.com/images/I/81VBPk5pnLL._AC_SL1500_.jpg\"},{\"id\":1,\"image\":\"https://images-na.ssl-images-amazon.com/images/I/81VBPk5pnLL._AC_SL1500_.jpg\"},{\"id\":1,\"image\":\"https://images-na.ssl-images-amazon.com/images/I/81VBPk5pnLL._AC_SL1500_.jpg\"},{\"id\":1,\"image\":\"https://images-na.ssl-images-amazon.com/images/I/81VBPk5pnLL._AC_SL1500_.jpg\"},{\"id\":1,\"image\":\"https://images-na.ssl-images-amazon.com/images/I/81VBPk5pnLL._AC_SL1500_.jpg\"},{\"id\":1,\"image\":\"https://images-na.ssl-images-amazon.com/images/I/81VBPk5pnLL._AC_SL1500_.jpg\"}]");
+//              }else{
+//                  reject( new Error('Servidor sin conexion'));
+//              }
+//              console.log(resolve);
+//          },1000)
+//      }).then(resultado =>{
+//          //Resultado tiene el valor de resolve 
+//          resultado = JSON.parse(resultado);
+//          setProducts(resultado);
+//      }).catch(error =>{
+//          //Error tiene el resultado de reject
+//          alert(error.message);
+//      })
+        
+        /**SUGAR SYNTAX  ASYNC AWAIT
+         * async hace que una funcion retorne una promesa
+         * await makes que una funcion espere por la promesa
+        */
+        
+        let ServerOnline = true;
+        let promesa = new Promise((resolve,reject)=>{
             setTimeout(()=>{
-                resolve("[{\"id\":1,\"image\":\"https://images-na.ssl-images-amazon.com/images/I/81VBPk5pnLL._AC_SL1500_.jpg\"},{\"id\":1,\"image\":\"https://images-na.ssl-images-amazon.com/images/I/81VBPk5pnLL._AC_SL1500_.jpg\"},{\"id\":1,\"image\":\"https://images-na.ssl-images-amazon.com/images/I/81VBPk5pnLL._AC_SL1500_.jpg\"},{\"id\":1,\"image\":\"https://images-na.ssl-images-amazon.com/images/I/81VBPk5pnLL._AC_SL1500_.jpg\"},{\"id\":1,\"image\":\"https://images-na.ssl-images-amazon.com/images/I/81VBPk5pnLL._AC_SL1500_.jpg\"},{\"id\":1,\"image\":\"https://images-na.ssl-images-amazon.com/images/I/81VBPk5pnLL._AC_SL1500_.jpg\"}]");
-                console.log(resolve);
-            },1000)
-        }).then(resolve =>{
-            resolve = JSON.parse(resolve);
-            setProducts(resolve);
+                if(ServerOnline){
+                    resolve("[{\"id\":1,\"image\":\"https://images-na.ssl-images-amazon.com/images/I/81VBPk5pnLL._AC_SL1500_.jpg\"},{\"id\":1,\"image\":\"https://images-na.ssl-images-amazon.com/images/I/81VBPk5pnLL._AC_SL1500_.jpg\"},{\"id\":1,\"image\":\"https://images-na.ssl-images-amazon.com/images/I/81VBPk5pnLL._AC_SL1500_.jpg\"},{\"id\":1,\"image\":\"https://images-na.ssl-images-amazon.com/images/I/81VBPk5pnLL._AC_SL1500_.jpg\"},{\"id\":1,\"image\":\"https://images-na.ssl-images-amazon.com/images/I/81VBPk5pnLL._AC_SL1500_.jpg\"},{\"id\":1,\"image\":\"https://images-na.ssl-images-amazon.com/images/I/81VBPk5pnLL._AC_SL1500_.jpg\"}]");
+                }else{
+                    reject( new Error('Servidor sin conexion'));
+                }
+            },3000);    
         });
-*/
+         let data = await promesa;
+         data = JSON.parse(data);
 
-      
-  
-    
+         setProducts(data);
 
 }
 
